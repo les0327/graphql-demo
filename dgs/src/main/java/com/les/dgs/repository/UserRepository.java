13c0a3624c405f21600ff481d17f233d5dc27423
Volodymyr Lisovyi <les.volodumur@gmail.com>
2021-11-11 15:31:00 +0200

    Init modules for comparison

--- dgs/src/main/java/com/les/dgs/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.les.dgs.repository;
-
-import com.les.dgs.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
--- experimental/src/main/java/com/les/graphqldemo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.les.graphqldemo.repository;
-
-import com.les.graphqldemo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
